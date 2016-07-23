2befc159f017049238fe1c214221738483636876
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2016-07-23 13:27:17 +0200

    Adding form to make vote and displays the results

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+
+# Polls Home Page
+def index(request):
+    page_title = "Polls Home Page"
+    template_name = "polls/index.html"
+    # query the last 5 questions
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    context = {'page_title': page_title,
+               'latest_question_list': latest_question_list,
+               'output': output
+               }
+    return render(request, template_name, context)
+
+
+# polls questions
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    template_name = "polls/results.html"
+    return render(request, template_name, {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=int(question_id))
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Vous n'avez pas fait de choix.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',
+                                    args=(question.id,)))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    template_name = 'polls/detail.html'
+    page_title = "Question %s" % question.id
+    context = {'question': question, 'page_title': page_title}
+    return render(request, template_name, context)
